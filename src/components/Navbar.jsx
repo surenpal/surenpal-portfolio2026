a7b2764ee3eca21d031848b88964d124b5af4680
surenpal <surenpal666@gmail.com>
2026-01-01 23:00:18 +0900

    react image is not showing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 export default function Navbar() {
   return (
-    <nav className="w-full p-4 bg-white shadow-md fixed top-0 left-0 z-50">
+    <nav className="w-full p-4 bg-indigo-400 shadow-md fixed top-0 left-0 z-50">
       <div className="max-w-6xl mx-auto flex justify-between items-center">
         <h1 className="text-xl font-bold">Suren Pal</h1>
         <div className="space-x-6">
           <a href="#about">About</a>
+          <a href="#mycareer">My Career</a>
           <a href="#skills">Skills</a>
           <a href="#projects">Projects</a>
           <a href="#contact">Contact</a>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,6 +4,7 @@ export default function Skills() {
       <h2 className="text-3xl font-bold mb-6">Skills</h2>
 
       <div className="flex flex-wrap gap-4">
+        <img src="./assets/react.png" alt="React" className="w-12 h-12" />
         <span className="px-4 py-2 bg-white shadow rounded">React</span>
         <span className="px-4 py-2 bg-white shadow rounded">Tailwind</span>
         <span className="px-4 py-2 bg-white shadow rounded">JavaScript</span>
