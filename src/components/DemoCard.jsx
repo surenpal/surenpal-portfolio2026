8e247e14864bd9bfded8f096cd25cbb4f5163c0b
surenpal <surenpal666@gmail.com>
2026-01-01 23:48:25 +0900

    ffffffffuuuuuuuuuuuuuuuuuuuuuuuuuuu11111111111111

--- src/components/DemoCard.jsx
+++ src/components/DemoCard.jsx
@@ -1,19 +1,11 @@
 export default function DemoCard() {
   return (
     <div className="w-64 bg-white rounded-xl overflow-hidden shadow-md hover:shadow-xl transition duration-300">
-      <div className="overflow-hidden">
+        <h2 className="text-lg font-semibold">Test-Card</h2>
         <img
-          src="https://picsum.photos/300"
+          src=".src/assets/MERN.png"
           className="w-full h-90 object-cover transition duration-300 hover:scale-105"
         />
-      </div>
-
-      <div className="p-4">
-        <h2 className="text-lg font-semibold">Beautiful Card</h2>
-        <p className="text-sm text-gray-600 opacity-0 transition duration-300 hover:opacity-100">
-          This is test for the demo card
-        </p>
-      </div>
     </div>
   );
 }
